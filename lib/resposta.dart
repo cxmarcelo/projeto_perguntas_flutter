070527fbbbb0d595b6791675e6bcbfead9786aac
Marcelo Carvalho <marcelo.c.b22@gmail.com>
2020-06-23 02:24:00 -0300

    Aplicando estilo e passando uma callback para o componente Resposta

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -3,15 +3,19 @@ import 'package:flutter/material.dart';
 class Resposta extends StatelessWidget {
 
   final String texto;
+  final void Function() onSelecionado;
 
-  Resposta(this.texto);
+  Resposta(this.texto, this.onSelecionado);
   
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       child: RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
               child: Text(texto),
-              onPressed: null,
+              onPressed: onSelecionado,
             ),    
     );
   }
